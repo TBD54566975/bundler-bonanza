acaea0ade38d620659a27f3f71431d4d2d45c99d
Nick DeJesus <ndejesus1227@gmail.com>
2023-09-22 13:06:02 -0400

    adding web5 connect function and displaying the did

--- docusaurus/src/pages/index.js
+++ docusaurus/src/pages/index.js
@@ -8,12 +8,29 @@ import { Web5 } from "@web5/api/browser";
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
+  const [web5, setWeb5] = React.useState(null);
+
+  React.useEffect(() => {
+    async function handleWeb5Connect() {
+      try {
+        const web5Instance = await Web5.connect();
+        console.log("Web5 connected");
+        setWeb5(web5Instance);
+      } catch (error) {
+        console.error("Failed to connect to Web5:", error);
+      }
+    }
+    handleWeb5Connect();
+  }, []);
+
+  console.log(web5);
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />"
     >
       <main>Web5 stuff right here</main>
+      {web5 && <div>{web5.did}</div>}
     </Layout>
   );
 }
