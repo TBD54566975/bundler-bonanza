2722bb7cc4b809968fa160cac58ed69b69e38d9e
Nick DeJesus <ndejesus1227@gmail.com>
2023-10-06 14:46:07 -0400

    update test to reflect dwn things

--- tests/node-esm/web5-test.js
+++ tests/node-esm/web5-test.js
@@ -1,14 +1,82 @@
-import { Web5 } from '@tbd54566975/web5';
+import { Web5 } from "@tbd54566975/web5";
 
-const { web5, did } = await Web5.connect({
-  techPreview: {
-    dwnEndpoints: ['http://localhost:3000']
+const checkWeb5 = async () => {
+  let result = {
+    web5: null,
+    did: null,
+    record: null,
+    readStatus: null,
+    updateStatus: null,
+    deleteStatus: null,
+  };
+
+  try {
+    result.web5 = await Web5.connect({
+      techPreview: {
+        dwnEndpoints: ["http://localhost:3000"],
+      },
+    });
+    did = result.web5.did;
+  } catch (error) {
+    console.log("Error:", error);
   }
-});
 
-const { record, status } = await web5.dwn.records.write({
-  data: 'Hello!'
-});
+  try {
+    const { record: _record } = await result.web5.dwn.records.write({
+      data: "Hello!",
+    });
+    result.record = _record;
+  } catch (error) {
+    console.log("Record Error:", error);
+  }
 
-console.log(JSON.stringify(status, null, 2));
-console.log(record);
+  try {
+    const { status } = await result.web5.dwn.records.read({
+      recordId: record.id,
+    });
+
+    result.readStatus = status;
+  } catch (error) {
+    console.log("Read Record Error:", error);
+  }
+
+  try {
+    const { status } = await result.web5.dwn.records.update({
+      recordId: record.id,
+      data: "Hello Update!",
+    });
+
+    result.updateStatus = status;
+  } catch (error) {
+    console.log("Update Record Error:", error);
+  }
+
+  try {
+    const { status } = await result.web5.dwn.records.delete({
+      recordId: record.id,
+    });
+
+    result.deleteStatus = status;
+  } catch (error) {
+    console.log("Delete Record Error:", error);
+  }
+
+  console.log(JSON.stringify(result, null, 2));
+
+  return result;
+};
+
+checkWeb5();
+
+// const { web5, did } = await Web5.connect({
+//   techPreview: {
+//     dwnEndpoints: ['http://localhost:3000']
+//   }
+// });
+
+// const { record, status } = await web5.dwn.records.write({
+//   data: 'Hello!'
+// });
+
+// console.log(JSON.stringify(status, null, 2));
+// console.log(record);
