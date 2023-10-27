93373322cdadfcc34c5e15c07d8c4cea7bfe2aa7
Nick DeJesus <ndejesus1227@gmail.com>
2023-10-27 16:10:52 -0400

    applied feedback

--- tests/browser-esbuild/bundle.js
+++ tests/browser-esbuild/bundle.js
@@ -7,11 +7,11 @@ import { createRequire } from "node:module";
 const require = createRequire(import.meta.url);
 
 esbuild.build({
-  entryPoints: ["dwn-sdk-test.js"],
+  entryPoints: ["web5-and-dwn-test.js"],
   platform: "browser",
   bundle: true,
   format: "esm",
-  outfile: "public/dist/dwn-sdk-test.js",
+  outfile: "public/dist/web5-and-dwn-test.js",
   inject: [require.resolve("node-stdlib-browser/helpers/esbuild/shim")],
   plugins: [polyfillProviderPlugin(stdLibBrowser)],
   define: {
