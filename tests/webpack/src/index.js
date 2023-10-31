50c123b18d204d3f0ce3cae75596d673d8a25ed1
Leo Ribeiro <leordev@gmail.com>
2023-10-31 15:48:46 -0400

    fix webpack

--- tests/webpack/src/index.js
+++ tests/webpack/src/index.js
@@ -1,7 +1,5 @@
-import { Buffer } from 'buffer';
+import { Buffer } from "buffer";
 
-import _ from 'lodash';
-// import { testDwn } from './dwn-sdk-test.js'\\
 import {
   Dwn,
   EventLogLevel,
@@ -13,24 +11,22 @@ import {
   RecordsDelete,
   DataStream,
   DidKeyResolver,
-} from '@tbd54566975/dwn-sdk-js';
-import { Web5 } from '@web5/api/browser';
+} from "@tbd54566975/dwn-sdk-js";
+import { Web5 } from "@web5/api";
 
-import checkDwn from '../../util/dwn-test';
-import checkWeb5 from '../../util/web5-test';
+import checkDwn from "../../util/dwn-test";
+import checkWeb5 from "../../util/web5-test";
 
-if (typeof window !== 'undefined') {
+if (typeof window !== "undefined") {
   window.Buffer = Buffer;
 }
 
 async function component() {
-  const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+  const element = document.createElement("div");
+  element.innerHTML = "hello webpack!";
   return element;
 }
+
 checkWeb5(Web5);
 checkDwn(
   Dwn,
--- tests/webpack/webpack.config.js
+++ tests/webpack/webpack.config.js
@@ -1,25 +1,31 @@
-const path = require('path');
+const path = require("path");
+const webpack = require("webpack");
 
 /** @type {import('webpack').Configuration} */
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  devtool: 'inline-source-map',
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist'),
+      directory: path.join(__dirname, "dist"),
     },
   },
   resolve: {
     fallback: {
-      stream: require.resolve('stream-browserify'),
-      crypto: require.resolve('crypto-browserify'),
-      buffer: require.resolve('buffer'),
+      stream: require.resolve("stream-browserify"),
+      crypto: require.resolve("crypto-browserify"),
+      buffer: require.resolve("buffer"),
     },
   },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: "process/browser.js",
+    }),
+  ],
 };
