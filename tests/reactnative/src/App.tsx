d787f7245071f0b955e54155110f1feaa67bb795
Leo Ribeiro <leonardo@squareup.com>
2023-10-30 21:28:12 -0400

    add web5 to rn (wip)

--- tests/reactnative/src/App.tsx
+++ tests/reactnative/src/App.tsx
@@ -1,14 +1,31 @@
 import React, { useEffect, useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { DwnManager } from "./DwnManager";
 
 import { Web5 } from "@web5/api";
+import { getWeb5 } from "./Web5Manager";
 
 export default function App() {
   const [dwnTestsResults, setDwnTestsResults] = useState("");
+  const [web5TestResults, setWeb5TestsResults] = useState("");
 
   useEffect(() => {
-    // TODO: add proper web5 tests
+    const loadWeb5AndTestsResults = async () => {
+      try {
+        const web5 = await getWeb5();
+        console.info({ web5 });
+      } catch (error) {
+        console.error(error);
+        const errorResults = {
+          success: false,
+          error: true,
+          message: error.message,
+        };
+        const errorResultsStr = JSON.stringify(errorResults, undefined, 2);
+        setWeb5TestsResults(errorResultsStr);
+        throw error;
+      }
+    };
 
     const loadDwnAndTestsResults = async () => {
       try {
@@ -30,6 +47,7 @@ export default function App() {
     };
 
     loadDwnAndTestsResults();
+    loadWeb5AndTestsResults();
   }, []);
 
   const onTestWeb5Press = async () => {
@@ -46,7 +64,14 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Bundling testapp for web5</Text>
-      <Button onPress={onTestWeb5Press} title="Test Web5" />
+      {web5TestResults && (
+        <>
+          <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+            Web5 Tests Result
+          </Text>
+          <Text testID="web5-tests-result">{web5TestResults}</Text>
+        </>
+      )}
       {dwnTestsResults && (
         <>
           <Text style={{ fontSize: 20, fontWeight: "bold" }}>
