515831c31bb69217fdafc2f0bce96d58fa40f2ca
Nick DeJesus <ndejesus1227@gmail.com>
2023-11-02 16:09:44 -0400

    update webpack to have web ui

--- tests/webpack/src/index.js
+++ tests/webpack/src/index.js
@@ -21,24 +21,75 @@ if (typeof window !== "undefined") {
   window.Buffer = Buffer;
 }
 
-async function component() {
+async function displayResults() {
   const element = document.createElement("div");
-  element.innerHTML = "hello webpack!";
+  element.innerHTML = "<h1>hello webpack!</h1>";
+
+  const web5ResultsElement = document.createElement("pre");
+  web5ResultsElement.id = "web5-results";
+  element.appendChild(web5ResultsElement);
+
+  const dwnResultsElement = document.createElement("pre");
+  dwnResultsElement.id = "dwn-results";
+  element.appendChild(dwnResultsElement);
+
+  try {
+    const web5Results = await checkWeb5(Web5);
+    const did = web5Results.web5.did.connectedDid;
+    web5ResultsElement.textContent = JSON.stringify(
+      {
+        success: true,
+        web5Results: did,
+      },
+      null,
+      2
+    );
+  } catch (error) {
+    web5ResultsElement.textContent = JSON.stringify(
+      {
+        success: false,
+        error: error.message,
+      },
+      null,
+      2
+    );
+  }
+
+  try {
+    const dwnResults = await checkDwn(
+      Dwn,
+      DataStream,
+      DidKeyResolver,
+      Jws,
+      RecordsWrite,
+      RecordsRead,
+      RecordsDelete,
+      MessageStoreLevel,
+      DataStoreLevel,
+      EventLogLevel
+    );
+    dwnResultsElement.textContent = JSON.stringify(
+      {
+        success: true,
+        dwnResults,
+      },
+      null,
+      2
+    );
+  } catch (error) {
+    dwnResultsElement.textContent = JSON.stringify(
+      {
+        success: false,
+        error: error.message,
+      },
+      null,
+      2
+    );
+  }
+
   return element;
 }
 
-checkWeb5(Web5);
-checkDwn(
-  Dwn,
-  DataStream,
-  DidKeyResolver,
-  Jws,
-  RecordsWrite,
-  RecordsRead,
-  RecordsDelete,
-  MessageStoreLevel,
-  DataStoreLevel,
-  EventLogLevel
-);
-
-document.body.appendChild(await component());
+(async () => {
+  document.body.appendChild(await displayResults());
+})();
